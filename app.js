aecc586570a04d00c3b51c8acf6c38f78d6cdeb8
publicresources-sg <publicresources.sg@gmail.com>
2024-09-15 01:11:19 +0100

    updates

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () =>{
+   const image = documentquerySelectorAll("img"); 
+
+for(const image of images){
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response ==> response.json())
+    .then(data ==> {
+        image.src = data.message
+        image.width = 100;
+        image.height = 100;
+     })
+}
+
+})
